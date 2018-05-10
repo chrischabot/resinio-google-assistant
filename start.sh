58b8c62507f25be553f104901db9e30ec1e8a471
Chris Chabot <chabotc@gmail.com>
2018-05-11 00:51:26 +0100

    .

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [ ! -f assistant.json ] ; then
+	echo $AS > assistant.json
+fi
+
+cd /usr/src/assistant
+source env/bin/activate
+python3 src/main.py
+
 while true; do
-	sleep 10
+	sleep 2
 	echo -ne "."
 done
