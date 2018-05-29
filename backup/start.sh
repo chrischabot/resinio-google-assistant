109e1e16e937cd9d2b754bcd8856f9000b8521cb
Chris Chabot <chabotc@gmail.com>
2018-05-29 16:54:03 +0100

    Alsa fixes

---
+++ backup/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f /root/assistant.json ] ; then
+	echo $AS > /root/assistant.json
+fi
+
+if [ ! -f /root/.cache/voice-recognizer/assistant_credentials.json ] ; then
+	mkdir -p /root/.cache/voice-recognizer/
+	echo $AC > /root/.cache/voice-recognizer/assistant_credentials.json
+fi
+
+cd /usr/src/app/assistant
+if [ ! -f pip_installed ] ; then
+	# six google-assistant-grpc google-cloud-speech google-auth-oauthlib
+	pip3 install -U google-assistant-library
+	touch pip_installed
+fi
+source env/bin/activate
+python3 src/main.py
